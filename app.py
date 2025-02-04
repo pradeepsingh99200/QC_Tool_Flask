e3fb7072de8ec2cd093e9957e52943b18d827581
pradeepsingh99200 <sign9867@gmail.com>
2025-02-04 17:15:15 +0530

    new updated

--- app.py
+++ app.py
@@ -138,4 +138,4 @@ def internal_server_error(e):
 if __name__ == '__main__':
     if not os.path.exists(UPLOAD_FOLDER):
         os.makedirs(UPLOAD_FOLDER)
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8000)
